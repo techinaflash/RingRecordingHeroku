6abd371f945c8db9d408947d3e17aba34a1448d0
techinaflash <matthew@techinaflash.net>
2019-09-04 21:47:31 -0400

    fix find user id
    added id to who initiated the slack action

--- src/index.js
+++ src/index.js
@@ -45,7 +45,7 @@ app.post('/slack/events', (req, res) => {
   if(type === 'message_action') {
     // Get user info of the person who posted the original message from the payload
     const getUserInfo = new Promise((resolve, reject) => {
-      users.find(payload.user).then((result) => {
+      users.find(payload.user.id).then((result) => {
         console.log('**********USER FIND RESULT*****************')
         console.log(result)
         resolve(result.data.user.profile.real_name);
